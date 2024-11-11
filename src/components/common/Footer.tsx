16dc14219ebf065070fa71b9978f24d6a8571795
Chaemin-Lim (임채민) <antraxmin@naver.com>
2024-11-11 17:43:46 +0900

    feat: global layout

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.footer`
+  display: grid;
+  place-items: center;
+  margin-top: auto;
+  padding: 50px 0;
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+`
+
+export default function Footer() {
+  return (
+    <Wrapper>
+      <div>Copyright © 2024 Antraxmin</div>
+    </Wrapper>
+  )
+}
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import {
+  AiFillGithub,
+  AiOutlineInstagram,
+  AiFillLinkedin,
+} from 'react-icons/ai'
+import React from 'react'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+`
+
+const Title = styled(Link)`
+  font-size: 15px;
+  font-weight: 700;
+  text-decoration: none;
+  color: inherit;
+`
+
+const Menu = styled.div`
+  display: flex;
+  gap: 15px;
+  font-size: 25px;
+
+  & > a {
+    display: flex;
+    color: initial;
+  }
+`
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <Title to="/">Antraxmin</Title>
+
+      <Menu>
+        <a href="#" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href="#" target="_blank">
+          <AiOutlineInstagram />
+        </a>
+        <a href="#" target="_blank">
+          <AiFillLinkedin />
+        </a>
+      </Menu>
+    </Wrapper>
+  )
+}
