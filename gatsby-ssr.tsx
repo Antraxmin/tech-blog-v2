55032aa37cf1e3f4443058427354d348c2c4b8f4
Chaemin-Lim (임채민) <antraxmin@naver.com>
2024-11-11 17:46:33 +0900

    gatsby ssr - onRenderBody API

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,18 @@
+import { GatsbySSR } from 'gatsby'
+import React from 'react'
+
+const HeadComponents = [
+  <link
+    rel="stylesheet"
+    as="style"
+    crossOrigin="anonymous"
+    href="<https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css>"
+    key="pretendard-font"
+  />,
+]
+
+export const onRenderBody: GatsbySSR['onRenderBody'] = ({
+  setHeadComponents,
+}) => {
+  setHeadComponents(HeadComponents)
+}
